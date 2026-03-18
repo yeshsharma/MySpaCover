7707238abea3bce4274dae5ce02d09921dbe4b17
yeshsharma <yeshsharma516032@gmail.com>
2026-03-18 13:19:30 +0530

    new test case

--- src/test/java/zasyasolutions/SpaCover/TestCases/Cart.java
+++ src/test/java/zasyasolutions/SpaCover/TestCases/Cart.java
@@ -70,7 +70,7 @@ public class Cart {
         WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions();
-        //options.addArguments("--headless=new");
+        options.addArguments("--headless=new");
         options.addArguments("--window-size=1920,1080");
         options.addArguments("--disable-gpu");
         options.addArguments("--no-sandbox");
